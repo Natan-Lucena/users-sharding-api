a633eec2b60a6147c181195115af0690dd7ecb05
Natan Lucena <zerogamerptbr@gmail.com>
2025-02-09 19:31:30 -0300

    :sparkles: add user registration schema and create user use case for handling user creation logic

---
+++ src/api/application/schemas/register-user.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { validationErrors } from "../../shared/core/validation-erros";
+
+export const registerUserSchema = z.object({
+  email: z
+    .string({ invalid_type_error: validationErrors.string("email") })
+    .email(),
+  password: z.string({
+    invalid_type_error: validationErrors.string("password"),
+  }),
+  name: z.string({ invalid_type_error: validationErrors.string("name") }),
+  taxId: z
+    .string({ invalid_type_error: validationErrors.string("taxId") })
+    .regex(new RegExp("^[0-9]+$"))
+    .min(11)
+    .max(14),
+});
---
+++ src/api/application/use-cases/create-user.ts
@@ -0,0 +1,38 @@
+import { User } from "../../domain/entities/user";
+import { IUserRepository } from "../../domain/repositories/user-repository";
+import { failure, Result, success } from "../../shared/core/result";
+import { hash } from "bcrypt";
+
+interface ICreateUser {
+  email: string;
+  name: string;
+  password: string;
+  taxId: string;
+}
+
+export class CreateUserUseCase {
+  constructor(private repository: IUserRepository) {}
+
+  async execute(
+    data: ICreateUser
+  ): Promise<Result<User, "USER_ALREADY_EXISTS">> {
+    const userAlreadyExists = await this.repository.search({
+      email: data.email,
+      taxId: data.taxId,
+    });
+
+    if (userAlreadyExists) {
+      return failure("USER_ALREADY_EXISTS");
+    }
+
+    const hashedPassword = hash(data.password, 10);
+    const user = User.create({
+      email: data.email,
+      name: data.name,
+      password: hashedPassword,
+      taxId: data.taxId,
+    });
+    const result = await this.repository.create(user);
+    return success(result);
+  }
+}

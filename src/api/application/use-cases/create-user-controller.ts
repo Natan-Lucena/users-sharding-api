b56d9006dc8a531ee0effdff7e904b749e42502f
Natan Lucena <zerogamerptbr@gmail.com>
2025-02-09 19:37:47 -0300

    :sparkles: add CreateUserController for handling user creation requests with validation

---
+++ src/api/application/use-cases/create-user-controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { BaseController } from "../../shared/infra/http/base-controller";
+import { CreateUserUseCase } from "./create-user";
+import { registerUserSchema } from "../schemas/register-user.schema";
+import { formatValidationErrors } from "../../shared/core/validation-erros";
+
+export class CreateUserController extends BaseController {
+  constructor(private useCase: CreateUserUseCase) {
+    super();
+  }
+
+  async executeImpl(req: Request, res: Response): Promise<Response | void> {
+    const validation = await registerUserSchema.safeParseAsync(req.body);
+    if (!validation.success) {
+      const errors = formatValidationErrors(validation.error);
+      return this.clientError(res, undefined, errors);
+    }
+
+    const payload = validation.data;
+
+    const result = await this.useCase.execute(payload);
+
+    if (!result.ok) {
+      return this.clientError(res, result.error);
+    }
+
+    return this.created(res, result.value);
+  }
+}

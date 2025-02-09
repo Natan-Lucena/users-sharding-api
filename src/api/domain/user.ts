a96e7dc80662a32df06251216ea5e886545e3c8b
Natan Lucena <zerogamerptbr@gmail.com>
2025-02-09 13:51:34 -0300

    :sparkles: add User class with creation and profile picture update functionality

---
+++ src/api/domain/user.ts
@@ -0,0 +1,59 @@
+import { Uuid } from "../shared/core/uuid";
+
+interface ICreateUser {
+  id?: Uuid;
+  createdAt?: Date;
+  profilePictureUrl?: string;
+  email: string;
+  name: string;
+  password: string;
+  taxId: string;
+}
+
+export class User {
+  constructor(
+    readonly id: Uuid,
+    private _email: string,
+    private _name: string,
+    private _password: string,
+    private _taxId: string,
+    readonly createdAt: Date,
+    private _profilePictureUrl?: string
+  ) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  get taxId(): string {
+    return this._taxId;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  get profilePictureUrl(): string | undefined {
+    return this._profilePictureUrl;
+  }
+
+  static create(props: ICreateUser): User {
+    return new User(
+      props.id || Uuid.random(),
+      props.email,
+      props.name,
+      props.password,
+      props.taxId,
+      props.createdAt || new Date(),
+      props.profilePictureUrl
+    );
+  }
+
+  updateUserProfilePicture(profilePictureUrl: string): void {
+    this._profilePictureUrl = profilePictureUrl;
+  }
+}

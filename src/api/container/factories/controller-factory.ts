d28e9354f9a26ccdc1734d17f1e2f1abbf1cc106
Natan Lucena <zerogamerptbr@gmail.com>
2025-02-09 20:09:13 -0300

    :sparkles: add ControllerFactory and UseCaseFactory for user creation handling

---
+++ src/api/container/factories/controller-factory.ts
@@ -0,0 +1,8 @@
+import { CreateUserController } from "../../application/use-cases/create-user-controller";
+import { UseCaseFactory } from "./use-case-factory";
+
+export class ControllerFactory {
+  static createUserController(): CreateUserController {
+    return new CreateUserController(UseCaseFactory.createUserUseCase());
+  }
+}
---
+++ src/api/container/factories/use-case-factory.ts
@@ -0,0 +1,31 @@
+import Redis from "ioredis";
+import { UserRepositoryImpl } from "../../infrastructure/repositories/user-repository-impl";
+import { PrismaShards } from "../../infrastructure/database/prisma";
+import { PrismaClient } from "@prisma/client/extension";
+import { RedisClient } from "../../infrastructure/database/redis";
+import { CreateUserUseCase } from "../../application/use-cases/create-user";
+
+const redis = new Redis(process.env.REDIS_URL as string);
+const redisClient = new RedisClient(redis);
+const prismaShards = new PrismaShards(
+  new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL_SHARD1 } },
+  }),
+  new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL_SHARD2 } },
+  }),
+  new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL_SHARD3 } },
+  }),
+  new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL_SHARD4 } },
+  })
+);
+
+const userRepository = new UserRepositoryImpl(redisClient, prismaShards);
+
+export class UseCaseFactory {
+  static createUserUseCase(): CreateUserUseCase {
+    return new CreateUserUseCase(userRepository);
+  }
+}

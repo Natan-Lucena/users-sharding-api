8bc5a8928710012fb15c45f27e00cc3130991b7c
Natan Lucena <zerogamerptbr@gmail.com>
2025-02-10 17:20:25 -0300

    :sparkles: add profilePictureUrl to user creation logic; enhance user data handling in UserRepositoryImpl

--- src/api/infrastructure/repositories/user-repository-impl.ts
+++ src/api/infrastructure/repositories/user-repository-impl.ts
@@ -23,6 +23,7 @@ export class UserRepositoryImpl implements IUserRepository {
         email: user.email,
         name: user.name,
         taxId: user.taxId,
+        profilePictureUrl: user.profilePictureUrl,
       },
     });
     await this.redisClient.saveCache(user.taxId, userUpdated);
@@ -32,6 +33,7 @@ export class UserRepositoryImpl implements IUserRepository {
 
   async create(user: User): Promise<User> {
     const userShard = this.generateShard(user.id);
+    console.log(userShard);
     const prisma = await this.prismaShards.getShard(userShard);
 
     const result = await prisma.user.create({

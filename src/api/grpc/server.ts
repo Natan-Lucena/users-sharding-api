e566acc3cf49a9cdfb28750a432266758c5a08eb
Natan Lucena <zerogamerptbr@gmail.com>
2025-04-03 10:03:29 -0300

    :sparkles: add gRPC server implementation and user service; define GrpcStatusCode enum for status handling

---
+++ src/api/grpc/server.ts
@@ -0,0 +1,34 @@
+import * as grpc from "@grpc/grpc-js";
+import { user } from "../../../generated/user";
+import { UserServiceImplementation } from "./services/get-user-service";
+
+export class GrpcServer {
+  private server: grpc.Server;
+  private readonly port: number = 50051;
+
+  constructor() {
+    this.server = new grpc.Server();
+    this.loadServices();
+  }
+
+  private loadServices() {
+    this.server.addService(
+      user.UnimplementedUserServiceService.definition,
+      new UserServiceImplementation()
+    );
+  }
+
+  public start() {
+    this.server.bindAsync(
+      `0.0.0.0:${this.port}`,
+      grpc.ServerCredentials.createInsecure(),
+      (err, port) => {
+        if (err) {
+          console.error("Failed to start gRPC server:", err);
+          return;
+        }
+        console.log(`🚀 gRPC Server is running on port ${port}`);
+      }
+    );
+  }
+}
